6ef7e6f59f29aee0167bf65cc013f00dac3677e3
Chewsday <z647623@gmail.com>
2025-04-30 12:49:55 +0800

    setup connection with mongoDB and auth-routes basic

---
+++ backend/routes/auth.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import { login, logout, signup } from '../controllers/auth.controller'
+
+
+const router = Router()
+
+
+router.post('singup', signup)
+router.post('login', login)
+router.post('logout', logout)
+
+
+
+export default router
--- backend/server.ts
+++ backend/server.ts
@@ -1,14 +1,24 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+import authRoutes from './routes/auth.routes'
+import connectMongoDB from './db/connectMongoDB'
 
 
 const PORT = process.env.PORT || 9000
 
-
+dotenv.config()
 const app = express()
+
+
+
+
+app.use('/api/auth', authRoutes)
 
 
 
 
 app.listen( PORT , () => {
   console.log(`SIR！You make it HERE！AT ${PORT}`)
+  connectMongoDB()
 })
